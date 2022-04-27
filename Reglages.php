eedfe11e9ba074a8e361f31c9701ca285b3a19ba
MaimouniMohamedAmine <maimouni.mohamedamine.solicode@gmail.com>
2022-04-27 13:07:08 +0000

    can keep checks & shows msg

--- Reglages.php
+++ Reglages.php
@@ -16,7 +16,7 @@
 //   die("Connection failed: " . $conn->connect_error);
 // }
 // echo "Connected successfully";
-
+// error_reporting(0);
 //Setting;s plugin
 function settings_plugin()
 {
@@ -26,13 +26,72 @@ function settings_plugin()
     add_action('admin_menu','settings_plugin');
  function main_form()
  {
+
+//Saving data into database & keeping it 
     $radio_btn_Nom = '';   
-     if(get_option("radio_btn_Nom")){
+    $radio_btn_Nom_false = '';   
+    $radio_btn_Prenom ='';
+    $radio_btn_Prenom_false ='';
+    $radio_btn_Date = '';
+    $radio_btn_Date_false = '';
+    $radio_btn_Email = '';
+    $radio_btn_Email_false = '';
+    $radio_btn_Numero = '';
+    $radio_btn_Numero_false = '';
+    $radio_btn_Message = '';
+    $radio_btn_Message_false = '';
+
+     if(get_option("radio_btn_Nom") == 'true')
+     {
         $radio_btn_Nom = 'checked';
      }
-    //$content = get_option( 'siteurl' );
-    //$editor_id = 'id';//I don't know why it works as it does not refer to anything according to wp_editor documentation. Is it the first instantiation??
-    //wp_editor($content,'id');
+     else
+     {
+        $radio_btn_Nom_false = 'checked';
+     }
+     
+     if (get_option("radio_btn_Prenom") == 'true')
+     {
+        $radio_btn_Prenom ='checked';
+     }
+     else
+     {
+        $radio_btn_Prenom_false ='checked';
+
+     }
+     if (get_option("radio_btn_Date") == 'true')
+     {
+        $radio_btn_Date = 'checked';
+     }
+     else
+     {
+        $radio_btn_Date_false = 'checked';
+     }
+     if (get_option("radio_btn_Email") == 'true')
+     {
+        $radio_btn_Email = 'checked';   
+     }
+     else
+     {
+        $radio_btn_Email_false = 'checked';
+     }
+     if (get_option("radio_btn_Numero") == 'true')
+     {
+        $radio_btn_Numero = 'checked';
+     }
+     else
+     {
+        $radio_btn_Numero_false = 'checked';
+     }
+     if (get_option("radio_btn_Message") == 'true')
+     {
+        $radio_btn_Message = 'checked';
+     }
+     else
+     {
+        $radio_btn_Message_false = 'checked';
+     }
+
    echo '<h1>Réglages</h1>
    <form method="POST" action="">
     <div id="form-group">
@@ -50,9 +109,9 @@ function settings_plugin()
                        <input name="wp_contact_nom"type="text">
                     </td>
                     <td>
-                       <input value="true" name="radio_btn_Nom" type="radio"'.$radio_btn_Nom.'>
+                       <input value="true" name="radio_btn_Nom" type="radio" '.$radio_btn_Nom.'>
                        <label for="">Activer</label>
-                       <input value="false" name="radio_btn_Nom" type="radio" >
+                       <input value="false" name="radio_btn_Nom" type="radio" '.$radio_btn_Nom_false.'>
                        <label for="">Désactiver</label>
                     </td>
                </tr>
@@ -62,21 +121,21 @@ function settings_plugin()
                        <input name="wp_contact_prenom"type="text">
                    </td>
                    <td>
-                       <input value="true" name="radio_btn_Prenom" type="radio">
+                       <input value="true" name="radio_btn_Prenom" type="radio"'.$radio_btn_Prenom.'>
                        <label for="">Activer</label>
-                       <input value="false" name="radio_btn_Prenom" type="radio" checked>
+                       <input value="false" name="radio_btn_Prenom" type="radio" '.$radio_btn_Prenom_false.'>
                        <label for="">Désactiver</label>
                    </td>
                </tr>
                <tr>
                    <td>
                        <label for=""> Date de naissance</label><br>
-                       <input name="wp_contact_date"type="date" name="" id="">
+                       <input name="wp_contact_date"type="date" name="" id="" >
                    </td>
                    <td>
-                       <input value="true" name="radio_btn_Date" type="radio">
+                       <input value="true" name="radio_btn_Date" type="radio" '.$radio_btn_Date.'>
                        <label for="">Activer</label>
-                       <input value="false" name="radio_btn_Date" type="radio" checked>
+                       <input value="false" name="radio_btn_Date" type="radio"'.$radio_btn_Date_false.'>
                        <label for="">Désactiver</label>
                    </td>
                </tr>
@@ -86,9 +145,9 @@ function settings_plugin()
                        <input name="wp_contact_email"type="email">
                    </td>
                    <td>
-                       <input value="true" name="radio_btn_Email" type="radio">
+                       <input value="true" name="radio_btn_Email" type="radio" '.$radio_btn_Email.'>
                        <label for="">Activer</label>
-                       <input value="false" name="radio_btn_Email" type="radio" checked>
+                       <input value="false" name="radio_btn_Email" type="radio"'.$radio_btn_Email_false.'>
                        <label for="">Désactiver</label>
                    </td>
                </tr>
@@ -98,9 +157,9 @@ function settings_plugin()
                        <input name="wp_contact_numero"type="number">
                    </td>
                    <td>
-                       <input value="true" name="radio_btn_Numero"type="radio">
+                       <input value="true" name="radio_btn_Numero"type="radio" '.$radio_btn_Numero.'>
                        <label for="">Activer</label>
-                       <input value="false" name="radio_btn_Numero"type="radio" checked>
+                       <input value="false" name="radio_btn_Numero"type="radio" '.$radio_btn_Numero_false.'>
                        <label for="">Désactiver</label>
                    </td>
                </tr>
@@ -110,9 +169,9 @@ function settings_plugin()
                        <input name="wp_contact_message"type="text">
                    </td>
                    <td>
-                       <input value="true" name="radio_btn_Message" type="radio">
+                       <input value="true" name="radio_btn_Message" type="radio" '.$radio_btn_Message.'>
                        <label for="">Activer</label>
-                       <input value="false" name="radio_btn_Message" type="radio" checked>
+                       <input value="false" name="radio_btn_Message" type="radio"'.$radio_btn_Message_false.'>
                        <label for="">Désactiver</label>
                    </td>
                </tr>
@@ -123,22 +182,21 @@ function settings_plugin()
        </div>
    </div>
 </form>';
-//Setting submit button to update the values inside the text editor
-$a = get_option( 'wp_main_form' );
-        print_r($a);
-if (isset($_POST['submit_btn_name']))  
-{
-                   //option name  | 
-    update_option('radio_btn_Nom',$_POST['radio_btn_Nom']);
-    update_option('radio_btn_Prenom',$_POST['radio_btn_Prenom']);
-    update_option('radio_btn_Date',$_POST['radio_btn_Date']);
-    update_option('radio_btn_Email',$_POST['radio_btn_Email']);
-    update_option('radio_btn_Numero',$_POST['radio_btn_Numero']);
-    update_option('radio_btn_Message',$_POST['radio_btn_Message']);
-    $value_radio_btn = $_POST['radio_btn_Nom'];
-    
 }
- }
+//Setting submit button to update the values inside the text editor
+    if (isset($_POST['submit_btn_name']))  
+    {
+                       //option name  | 
+        update_option('radio_btn_Nom',$_POST['radio_btn_Nom']);
+        update_option('radio_btn_Prenom',$_POST['radio_btn_Prenom']);
+        update_option('radio_btn_Date',$_POST['radio_btn_Date']);
+        update_option('radio_btn_Email',$_POST['radio_btn_Email']);
+        update_option('radio_btn_Numero',$_POST['radio_btn_Numero']);
+        update_option('radio_btn_Message',$_POST['radio_btn_Message']);
+
+        echo '<div class="updated"> The operation was completed successfully!!!!</div>';
+        
+    }
 //Return value function
 //  function return_value()
 //  {
@@ -152,6 +210,13 @@ if (isset($_POST['submit_btn_name']))
 // }
    ?>
    <style>
+       .updated
+       {    
+        margin-left:13em !important;
+        background-color:#cee6d4 !important;
+        color: #000!important;
+
+       }
       #submit_btn_id
       {  
         margin-top: 2em;    
